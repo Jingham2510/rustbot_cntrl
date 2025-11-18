5e909f8ba669908e2e26cd62f0812827115bf7c1
Jingham2510 <jsphingham@gmail.com>
2025-11-18 22:01:09 +0000

    Implement a PD controller (maybe)

--- src/control/force_control/force_control.rs
+++ src/control/force_control/force_control.rs
@@ -1,5 +1,7 @@
 use anyhow::bail;
 
+use chrono;
+use chrono::{DateTime, Local};
 //NOTE - all control functions must have a footprint of fn function_name(err: f32)->Result<f32, anyhow::Error>
 //This is to comply with the callback function used in the main test procedure
 
@@ -17,6 +19,45 @@ pub fn polarity_step_control(err :f32) -> Result<f32, anyhow::Error>{
         Ok(-STEP_VALUE)
     } else {
         Ok(0.0)
+    }
+}
+
+
+
+//Impleemtns a proportional gain controller based on error magnitude
+pub fn prop_gain_control(err:f32) -> Result<f32, anyhow::Error>{
+
+    //How far the step should be if the error is a value of 1N
+    const BASE_STEP :f32 = 0.1;
+
+    Ok(BASE_STEP * err)
+
+}
+
+
+struct PdController {
+    prev_err : f32,
+    prev_time : DateTime<Local>
+}
+
+impl PdController {
+
+    pub fn calc_op(&mut self, err:f32) ->Result<f32, anyhow::Error>{
+
+        //gain
+        const KD_GAIN : f32 = 1.0;
+
+        //Get current time
+        let now = chrono::offset::Local::now();
+
+        //Calculate the derivative of the current slope
+        let derr = (err - self.prev_err)/((now - self.prev_time).as_seconds_f32());
+
+        //Update the previous values
+        self.prev_time = now;
+        self.prev_err = err;
+
+        Ok(derr)
     }
 
 }
